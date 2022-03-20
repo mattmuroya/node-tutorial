d301def3a0d330f2d1a0bd6bd07d993d63fb5ba2
mattmuroya <matt.muroya@gmail.com>
2022-03-20 13:09:53 -1000

    basic routing and status code info

---
+++ routing-statusCodes/server.js
@@ -0,0 +1,41 @@
+const http = require('http'); // import http module
+const fs = require('fs');
+
+const server = http.createServer((req, res) => { // creates instance of server in server variable; req object contains info about the request; res object used to send response to the user
+  console.log(req.url, req.method) // this callback runs every time a request is made to this server
+
+  res.setHeader('Content-Type', 'text/html'); // set header content type for content being sent back to browser
+
+  let path = './views/'; // get the URL from the request
+  switch(req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200; // setting the status code to send back; 200 = everything is ok
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-me':
+      res.setHeader('Location', '/about'); // redirect
+      res.statusCode = 301; // 'resource has been permanently moved'
+      res.end();
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404; // resource doesn't exist
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data); // if writing multiple things, use res.write(data1); res.write(data2); etc etc and then res.end();
+    }
+  });
+});
+
+server.listen(3000, 'localhost', () => { // server must listen for incoming requests; takes in port number to listen to; 'localhost' name is default but can be specified
+  console.log('now listening for requests on port 3000') // callback fires when the server starts listening.
+});
